ab5dd0cfb8306df0b6d73130b1a138e05897a41c
Siddhant Chaudhary <urssidd@gmail.com>
2021-02-21 22:52:48 +0530

    v0.0.0

--- template.cpp
+++ template.cpp
@@ -50,6 +50,8 @@ template <class T> ll maxpower(T a , T b){ll ans = 0;while(a > 0 && a % b == 0){
 template <class T> T extgcd(T a, T b , T &x , T &y) {x = 1, y = 0;T x1 = 0, y1 = 1, a1 = a, b1 = b; while (b1) { T q = a1 / b1; tie(x, x1) = make_tuple(x1, x - q * x1); tie(y, y1) = make_tuple(y1, y - q * y1); tie(a1, b1) = make_tuple(b1, a1 - q * b1);}return a1;}
 template <class T> T modinv (T a , T m , T &x , T &y){T g = extgcd(a , m , x , y); if (g != 1) cerr << "Division by zero!"; else x = (x % m + m) % m; return x;}
 
+//define global variables here
+
 void solve(){
 
 }
@@ -81,6 +83,7 @@ int main(){
             t--;
         }
     */
+    cout << "hello";
     cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << "seconds" << "\n";
     return 0;
 }
