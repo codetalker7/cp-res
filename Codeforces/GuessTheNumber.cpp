5c2663ff9a0a7a6c4dded5cdd8479f3dc430de91
Siddhant Chaudhary <urssidd@gmail.com>
2021-03-01 12:43:24 +0530

    more problems

---
+++ Codeforces/GuessTheNumber.cpp
@@ -0,0 +1,98 @@
+    /*
+        template-by: codetalker7
+        editor: sublime text 3
+        problem: https://codeforces.com/gym/101021/problem/1
+    */
+    #include<iostream>
+    #include<vector>
+    #include<string>
+    #include<algorithm>
+    #include<stack>
+    #include<unordered_set>
+    #include<cmath>
+    #include<numeric>
+    #include<iterator>
+    #include<set>
+    #include<unordered_map>
+    #include<map>
+    #include<queue>
+    #include<cstring>
+    #include<stdio.h>
+    #include<fstream>
+    #include<iomanip>
+    #include<functional>
+    #include<bitset>
+    #include<chrono>
+    #include<climits>
+    #include<assert.h>
+    using namespace std;
+
+    //type declarations
+    using ll = long long int;
+    using ull = unsigned long long int;
+    using vint = vector<int>;
+    using vll = vector<ll>;
+    using vch = vector<char>;
+    using vs = vector<string>;
+    using sll = set<ll>;
+    using msll = multiset<ll>;
+    using ldb = long double;
+
+    //constants
+    const ll MOD = 1000000007;
+    const ll INF = 1e18;
+    const int INFINT = 1e9 + 5;
+    const ldb PI = 3.14159265359;
+
+    //some useful algos
+    template <class T> T mceil(T a, T b){return (a % b == 0) ? a/b : a/b + 1;}
+    template <class T> T expo(T base , T exp , T mod){T res = 1;base = base % mod;while (exp > 0){if (exp & 1)res = (res*base) % mod;exp = exp>>1;base = (base*base) % mod;}return res;}
+    template <class T> ll maxpower(T a , T b){ll ans = 0;while(a > 0 && a % b == 0){ans++;a /= b;}return ans;}
+    template <class T> T extgcd(T a, T b , T &x , T &y) {x = 1, y = 0;T x1 = 0, y1 = 1, a1 = a, b1 = b; while (b1) { T q = a1 / b1; tie(x, x1) = make_tuple(x1, x - q * x1); tie(y, y1) = make_tuple(y1, y - q * y1); tie(a1, b1) = make_tuple(b1, a1 - q * b1);}return a1;}
+    template <class T> T modinv (T a , T m , T &x , T &y){T g = extgcd(a , m , x , y); if (g != 1) cerr << "Division by zero!"; else x = (x % m + m) % m; return x;}
+
+    //define global variables here
+
+    void solve(){
+
+    }
+
+    //main function
+    int main(){
+        //faster io
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+
+    /*
+        if there is an online judge, we use the
+        standard io streams. Otherwise, we use custom
+        io files.
+    */
+
+
+        //for testcases, use the below format
+        /*
+        ll t; //testcases
+        cin >> t;
+        while(t > 0){
+            solve(); //write a separate solve function
+            t--;
+        }
+        */
+        ll low = 1 , high = 1e6;
+        while(low <= high){
+            ll temp = low + (high - low)/2;
+            printf("%lld\n" , temp);
+            fflush(stdout);
+
+            char temp_response[100];
+            scanf("%s" , &temp_response);
+            string response = temp_response;
+            if (response.compare("<") == 0)
+                high = temp - 1;
+            else
+                low = temp + 1;
+        }
+        printf("! %lld" , low - 1);
+        return 0;
+    }
